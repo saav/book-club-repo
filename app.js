62bd3baf6c21dfa4ec571fc7a110d54960ce4361
savin <savin.varshney@gmail.com>
2024-09-15 13:23:29 +0800

    added updates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const messages = require('./messages');
 const bodyParser = require('body-parser');
+const books = [];
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,10 +16,25 @@ app.get('/about', (req, res) => {
 app.use((req, res) => {
  res.status(404).send(messages.notFound);
 });
-app.post('/submit', (req, res) => {
+
+app.post('/submit', (req, res, next) => {
   const book = req.body.book;
+  if (!book) {
+    const err = new Error('Book title is required');
+    return next(err);
+  }
+  books.push(book);
   console.log(`Book submitted: ${book}`);
   res.send(`Book submitted: ${book}`);
+});
+
+app.get('/books', (req, res) => {
+  res.send(books.join(', '));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something Went Wrong!');
 });
 
 const port = 3000;
